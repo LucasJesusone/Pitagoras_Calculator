c976227c1265914f8f2e9a41c2eb3df0f43d0bd6
lucasjesusone <lucasjesusone@gmail.com>
2021-02-28 21:54:17 -0400

    Criando estilização global

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Main from "../src/components/Main";
+
+function App() {
+  return (
+    <div className="container">
+      <Main />
+    </div>
+  );
+}
+
+export default App;
